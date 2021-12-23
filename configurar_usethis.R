b777315d36da5c8d144a505eab894438ca9a3b39
uniesecruz <uniesecruz>
2021-12-23 09:51:25 -0300

    Initial commit

---
+++ configurar_usethis.R
@@ -0,0 +1,10 @@
+library(usethis)
+
+usethis::use_git_config(user.name="uniesecruz",
+                        user.email="uniesecruz")
+
+usethis::edit_r_environ()
+
+usethis::browse_github_token()
+
+usethis::create_github_token()
